d254d026f572da6b69c20d813117c64b5ec42b80
AlexSuspis <alexandresuspiro@gmail.com>
2021-02-01 18:30:21 +0000

    added skeleton API endpoints for GET, PUT, DELETE requests. PATCH endpoint now parses request body to find an object. If that object's key is also in the target task, replace task's value with value of new object

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { v4: uuid } = require('uuid');
+const bodyParser = require('body-parser')
 const port = 3000;
-app.set('view engine', 'ejs')
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded())
+app.use(bodyParser.urlencoded({extended : true}))
 
 const tasks = [
     {
-        id: uuid(),
+        id: 1,
         text: 'test test',
         subtasks: [],
         labels: [],
@@ -17,11 +21,34 @@ const tasks = [
 ]
 
 // GET all tasks
-app.get('/', (req, res) => {
-    res.send("hello world")
+app.get('/tasks', (req, res) => {
+    res.render('tasks/index', { tasks })
 })
 
+// PATCH text
+// PATCH array of substasks
+// PATCH array of labels
+// PATCH colour
+app.patch('/tasks/:id', (req, res) => {
+    let { id } = req.params;
 
+    let [newProperty, newValue] = Object.entries(req.body)[0];
+
+    let task = tasks.find(t => t.id === parseInt(id));
+
+    for(let property in task){
+        if(newProperty == property){
+            task[property] = newValue;
+        }
+    }
+    console.dir(tasks[0])
+    res.send('patch req received!');
+})
+
+// DELETE a task
+app.delete('/tasks/:id', (req, res) => {
+    //we take in an id and remove the respective task from the array
+})
 
 
 app.listen(port, () => {
