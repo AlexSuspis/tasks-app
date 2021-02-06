2b04c4f630e82c7885ad2010c8d5e4051625a4d6
AlexSuspis <alexandresuspiro@gmail.com>
2021-02-06 16:11:59 +0000

    Added /public/setEventListeners.js - contains all events which will eventually be wired to respective HTML elements, and the logic for handling them, i.e. AXIOS requests.

---
+++ public/setEventListeners.js
@@ -0,0 +1,36 @@
+    //UI Events
+//Expand task options (Click on options icon)
+//
+
+
+    // AXIOS PATCH REQUESTS
+
+// Task’s text is modified (1)
+    //Trigger: A change in input element is detected. Then validated. Only then 
+    //is the AXIOS request sent.
+
+// Task’s colour is changed (2)
+    //Trigger: Colour pallette pops up when colour icon is clicked in 
+    //expanded OverlayMenu. When a colour is successfully saved, PATCH request sent.
+
+// Task’s subtask has been completed (3)
+
+// Label is added to task (4)
+
+// Subtask has been created (5)
+
+// Subtask has been completed (6)
+
+// Task’s order is changed (7)
+
+
+
+    //AXIOS DELETE REQUESTS
+
+// Task has been deleted (8)
+    //Trigger: Trash icon in task's overlay menu is clicked
+
+// Task has been completed (9)
+    //Trigger: Circular button in task's object has been clicked
+
+
