14d55042398d6c7cdec9dc743517dc945bbe1f70
AlexSuspis <alexandresuspiro@gmail.com>
2021-02-08 06:32:39 +0000

    created optionsOverlay component. It's a div with 4 spans, each containing the respective icon. Created and linked taskOptionsOverlay.css to index.ejs, so I can style optionsOverlay in a modular way. Next step is to wire the events to each of the icons inside optionsOverlay div

--- public/addTaskComponent.js
+++
@@ -1,7 +0,0 @@
-
-                    <ul>
-                        <%for(subtask of task.subtasks){%>
-                            <li><%=subtask.text%></li>
-                            <span><img src="/pen.svg" alt="" height="15px" width="15px"></span>
-                        <%}%>
-                    </ul>
