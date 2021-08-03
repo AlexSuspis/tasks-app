f799a2081f9c140266dac28fdbb5c1b37834d69a
Alexandre Suspiro <alex@Alexandres-MacBook-Air.local>
2021-08-03 15:27:22 +0100

    added basic drag and drop events. We can now drag tasks and drop them in another task container. The drop() event finds both the dragged task and the target task ids, so we can now swap them in swapTaskPositions.

--- public/js/taskDragAndDropEvents.js
+++ public/js/taskDragAndDropEvents.js
@@ -1,5 +1,3 @@
-const dragHandles = document.querySelectorAll('.myDragHandle');
-
 findAncestorElementWithId = (startElement, targetId) => {
 
     let ancestor = startElement.parentElement;
@@ -10,4 +8,60 @@ findAncestorElementWithId = (startElement, targetId) => {
     } while ((ancestor = ancestor.parentElement) !== null);
 
     return null;
+}
+swapTaskPositions = (task1, task2) => {
+
+}
+
+const dragHandles = document.querySelectorAll('.myDragHandle');
+dragHandles.forEach(handle => handle.addEventListener('dragstart', dragStart));
+
+function dragStart(e) {
+    const task = findAncestorElementWithId(e.target, "task");
+    const taskId = task.getAttribute('data-task_id');
+    console.log(taskId);
+    e.dataTransfer.setData("text/plain", taskId);
+    // console.log(e.dataTransfer.getData("text/plain"));
+}
+
+const taskContainers = document.querySelectorAll(".taskContainer");
+taskContainers.forEach(div => {
+    div.addEventListener('dragenter', dragEnter);
+    div.addEventListener('dragover', dragOver);
+    div.addEventListener('dragleave', dragLeave);
+    div.addEventListener('drop', drop);
+});
+
+
+
+function dragEnter(e) {
+    // console.log('dragenter');
+    //preventDefault() so we make the div a valid drop target
+    e.preventDefault();
+}
+function dragOver(e) {
+    // console.log('dragover');
+    //preventDefault() so we make the div a valid drop target
+    e.preventDefault();
+}
+function dragLeave(e) {
+    // console.log('dragleave');
+}
+function drop(e) {
+    //to stop redirecting to taskId
+    e.preventDefault();
+    const taskId = e.dataTransfer.getData('text/plain');
+
+    // const draggedTask = document.querySelector(`data-task_id[${taskId}]`);
+    const tasks = document.querySelectorAll("#task");
+    let draggedTask;
+    for (let task of tasks) {
+        if (task.getAttribute("data-task_id") === taskId) {
+            draggedTask = task;
+        }
+    }
+
+    const targetTask = findAncestorElementWithId(e.target, "task");
+
+    swapTaskPositions(draggedTask, targetTask);
 }
