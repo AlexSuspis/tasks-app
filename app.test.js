fb28271bcb8418343b1ea2dfda077995f182ef6f
Alex Suspiro <alexandresuspiro@gmail.com>
2021-08-26 14:27:26 +0100

    restructure test suites and correct wording

--- app.test.js
+++ app.test.js
@@ -1,14 +1,18 @@
 const request = require('supertest');
 const app = require('./app.js');
 
-describe("PATCH /task/:id/promote", () => {
-    it('should throw 404 if task ID in req.params is not found', () => { })
-    it('should throw 404 if task ID in req.body is not found', () => { })
+describe("Database queries", () => {
+    it('should throw 404 if task ID in req.params is not found in database', () => { })
+    it('should throw 404 if task ID in req.body is not found in database', () => { })
     it('should search database and find task1 and task2 by ID', () => { })
     it('should search database and find task1 and task2 by ID', () => { })
+})
 
+//a promotion means a subtask is promoted to a task
+describe("Promote a subtask to a task via PATCH /task/:id/promote", () => {
     //task1 is taskDraggedByUser and task2 is taskDroppedOnByUser
-    it('should delete parentTask attribute from task1', () => { })
+    it('should update task1\'s parentTask ID attribute to be null', () => { })
+    it('should delete task1\'s ID from task1\'s parentTask subtask array', () => { })
     it('should push task1\'s id to task2\`s subtasks array property', () => { })
     it('should save task1 and task2 onto database', () => { })
 })
